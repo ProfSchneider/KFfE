1fe8fd69a68863ffaf46184b66e4774f19c682ed
Prof. Dr. Ulrich Schneider <Prof.Schneider@online.de>
2016-11-13 17:57:18 +0100

    Quelltext kommentiert

--- 14_ExtendedKalmanFilter/14_2_EulerEKF/TestEulerEKF.m
+++ 14_ExtendedKalmanFilter/14_2_EulerEKF/TestEulerEKF.m
@@ -1,12 +1,12 @@
 %****************************************************************
-% Modul			  : TestEulerKalman.m                           *
-%				                            					*
+% Modul			  : TestEulerKalman.m                   *
+%				                            	*
 % Datum           : 02-Dez-2013                                 *
 %                                                               *
 % Funktion        : Lagebestimmung mit Kalman-Filter            *
 %                   (Startfunktion)                             *
 %                                                               *
-% Implementation  : MATLAB 2016a                  			    *
+% Implementation  : MATLAB 2016a                  		*
 %                                                               *
 % Author          : (c) 2011 Phil Kim, Alle Rechte vorbehalten. *
 %                   Bearbeitet von Prof. Schneider              *
